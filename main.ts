138edc7e3f4c6fd0d93b5e5d57244f48156b8415
supremeSanaya <60713417+supremeSanaya@users.noreply.github.com>
2020-02-05 13:44:46 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+let mySprite = sprites.create(img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c 1 f b b 1 f c b b c c c . . 
+. c f 1 b b f 1 b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b c b b b c b b b b f . . . . 
+f b 1 f f f 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, SpriteKind.Player)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100003060604060606060306060606060406060606060606060606060605060606060606060606040605060606060603060604060603060606060604060606060604060606060605060606060606050606060605060606060606060606060606060606060606060606060306060604060606060606040606060606060606060606030606060606050606060506060606060606030606060606060606060406060506060606060606060306060606060606060606060406060606060606060606060406060606060605060606060506030606030605060606060606040606060606060606060606030606060606060606060606060406060606060506060306040603`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.builtin.forestTiles23,sprites.builtin.forestTiles20,sprites.castle.saplingOak,sprites.castle.shrub,sprites.castle.saplingPine,sprites.castle.tileGrass2],
+            TileScale.Sixteen
+        ))
+scene.setBackgroundColor(7)
+controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(mySprite)
