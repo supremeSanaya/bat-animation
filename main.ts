18c560f6524cc140b23ef9e6a8d9fed0be26cf08
supremeSanaya <60713417+supremeSanaya@users.noreply.github.com>
2020-02-05 21:45:51 -0600

    add animation with up and left buttons

--- main.ts
+++ main.ts
@@ -24,7 +24,44 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
-let mySprite = sprites.create(img`
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+. f f f . . . . . . . . f f f . 
+f f c . . . . . . . f c b b c . 
+f c c . . . . . . f c b b c . . 
+c f . . . . . . . f b c c c . . 
+c f f . . . . . f f b b c c . . 
+f f f c c . c c f b c b b c . . 
+f f f c c c c c f b c c b c . . 
+. f c 3 c c 3 b c b c c c . . . 
+. c b 3 b c 3 b b c c c c . . . 
+c c 1 f b b 1 f b b c c . . . . 
+c b f 1 b b f 1 b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+f b c b b b c b b b b f . . . . 
+. f 1 f f f 1 b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`,img`
+. . f f f . . . . . . . . . . . 
+f f f c c . . . . . . . . f f f 
+f f c c c . c c . . . f c b b c 
+f f c 3 c c 3 c c f f b b b c . 
+f f c 3 b c 3 b c f b b c c c . 
+f c b b b b b b c f b c b c c . 
+c c 1 b b b 1 b c b b c b b c . 
+c b b b b b b b b b c c c b c . 
+c b 1 f f 1 c b b c c c c c . . 
+c f 1 f f 1 f b b b b f c . . . 
+f f f f f f f b b b b f c . . . 
+f f 2 2 2 2 f b b b b f c c . . 
+. f 2 2 2 2 2 b b b c f . . . . 
+. . f 2 2 2 b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,img`
 . . f f f . . . . . . . . f f f 
 . f f c c . . . . . . f c b b c 
 f f c c . . . . . . f c b b c . 
@@ -32,45 +69,24 @@ f c f c . . . . . . f b c c c .
 f f f c c . c c . f c b b c c . 
 f f c 3 c c 3 c c f b c b b c . 
 f f b 3 b c 3 b c f b c c b c . 
-. c 1 f b b 1 f c b b c c c . . 
-. c f 1 b b f 1 b c c c c . . . 
+. c 1 b b b 1 b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
 c b b b b b b b b b c c . . . . 
-c b c b b b c b b b b f . . . . 
-f b 1 f f f 1 b b b b f c . . . 
-f b b b b b b b b b b f c c . . 
-. f b b b b b b b b c f . . . . 
+c b 1 f f 1 c b b b b f . . . . 
+f f 1 f f 1 f b b b b f c . . . 
+f f 2 2 2 2 f b b b b f c c . . 
+. f 2 2 2 2 b b b b c f . . . . 
 . . f b b b b b b c f . . . . . 
 . . . f f f f f f f . . . . . . 
-`, SpriteKind.Player)
-tiles.setTilemap(tiles.createTilemap(
-            hex`1000100003060604060606060306060606060406060606060606060606060605060606060606060606040605060606060603060604060603060606060604060606060604060606060605060606060606050606060605060606060606060606060606060606060606060606060306060604060606060606040606060606060606060606030606060606050606060506060606060606030606060606060606060406060506060606060606060306060606060606060606060406060606060606060606060406060606060605060606060506030606030605060606060606040606060606060606060606030606060606060606060606060406060606060506060306040603`,
-            img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`,
-            [myTiles.tile0,sprites.builtin.forestTiles23,sprites.builtin.forestTiles20,sprites.castle.saplingOak,sprites.castle.shrub,sprites.castle.saplingPine,sprites.castle.tileGrass2],
-            TileScale.Sixteen
-        ))
-scene.setBackgroundColor(7)
-controller.moveSprite(mySprite, 100, 100)
-scene.cameraFollowSprite(mySprite)
-animation.runImageAnimation(
-mySprite,
-[img`
+`],
+    100,
+    false
+    )
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
 . . f f f . . . . . . . . . . . 
 f f f c c . . . . . . . . f f f 
 f f c c . . c c . . . f c b b c 
@@ -122,6 +138,52 @@ f b c b b b c b b b b f . . . .
 . . f b b b b b b c f . . . . . 
 . . . f f f f f f f . . . . . . 
 `],
-500,
-true
-)
+    100,
+    false
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c 1 f b b 1 f c b b c c c . . 
+. c f 1 b b f 1 b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b c b b b c b b b b f . . . . 
+f b 1 f f f 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, SpriteKind.Player)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100003060604060606060306060606060406060606060606060606060605060606060606060606040605060606060603060604060603060606060604060606060604060606060605060606060606050606060605060606060606060606060606060606060606060606060306060604060606060606040606060606060606060606030606060606050606060506060606060606030606060606060606060406060506060606060606060306060606060606060606060406060606060606060606060406060606060605060606060506030606030605060606060606040606060606060606060606030606060606060606060606060406060606060506060306040603`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.builtin.forestTiles23,sprites.builtin.forestTiles20,sprites.castle.saplingOak,sprites.castle.shrub,sprites.castle.saplingPine,sprites.castle.tileGrass2],
+            TileScale.Sixteen
+        ))
+scene.setBackgroundColor(7)
+controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(mySprite)
